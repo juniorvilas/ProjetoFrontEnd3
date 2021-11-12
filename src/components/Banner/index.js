dbf128f11ce5c4f446ec99ef41375f450e9717d6
juniorvilas <log_junior@hotmail.com>
2021-11-12 08:50:07 -0300

    alteracoes de divs

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -5,7 +5,10 @@ const Banner = () => {
     return (
            <section id="banner-bg">
             <div className="banner-jr">
-                <img src="https://scontent.fssa3-1.fna.fbcdn.net/v/t1.6435-9/61463320_10219882683485391_2298829516597886976_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=84a396&_nc_eui2=AeEczGaRquW79LHFzUVFF25PzAEAQgPmZuzMAQBCA-Zm7N9W7O1urmzCTSJC5IPguWE&_nc_ohc=dOi0qiDG6lEAX861pLo&tn=0boelGkfJ7D8b38-&_nc_ht=scontent.fssa3-1.fna&oh=7f1e99f966c657d988c269b68fc9bf83&oe=61B200E3" alt=""/>
+                <div>
+                    <img src="https://scontent.fssa3-1.fna.fbcdn.net/v/t1.6435-9/61463320_10219882683485391_2298829516597886976_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=84a396&_nc_eui2=AeEczGaRquW79LHFzUVFF25PzAEAQgPmZuzMAQBCA-Zm7N9W7O1urmzCTSJC5IPguWE&_nc_ohc=dOi0qiDG6lEAX861pLo&tn=0boelGkfJ7D8b38-&_nc_ht=scontent.fssa3-1.fna&oh=7f1e99f966c657d988c269b68fc9bf83&oe=61B200E3" alt=""/>
+                </div>
+                
                 <div>
                     <h2>Sobre mim</h2>
                     <p>Atualmente em mudança de área , desde do começo de Janeiro de 2021 estudando programação e em Maio de 2021 iniciei o curso Certified Tech Developer para ser Full Stack Jr com a Digital House!</p>
