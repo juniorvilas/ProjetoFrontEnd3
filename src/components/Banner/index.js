e860274684285adad88824c764b63480be57884c
Carlindo Silva Queiroz Junior <log_junior@hotmail.com>
2022-09-16 11:30:27 -0300

    Update index.js

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -11,18 +11,19 @@ const Banner = () => {
                             <a href="https://www.linkedin.com/in/carlindo-junior-556371197/">
                                 <img src={Junior} alt="Imagem Junior"/>
                             </a>                    
-                        </div>                
-                        <div className="banner-texto">                                
-                        <div>
-                            <h2>Sobre mim</h2>
-                            <p>
-                                Olá sou Carlindo Junior! <br /> Sou <b> Desenvolvedor Front-End </b> , construindo projetos com Javascript/(React,Next, TypeScript, React Query, Styled Components).
-                                Finalizei o primeiro ano do curso da DH em 05/2021 como full stack jr e atualmente faço especialização em Frontend a noite.
-                                Venho da área de logística, porém com mais de 2 anos de experiência no desenvolvimento de aplicações web.
-                                Tenho um olhar aguçado para negócios, novas metodologias.
-                                Sinta-se à vontade para entrar em contato comigo a qualquer momento, abraços!
-                            </p>
                         </div>
+
+                        <div className="banner-texto">                           
+                                <h2>Sobre mim</h2>
+                                <p className="about-me">
+                                    Olá sou Carlindo Junior! <br />Sou <b>Desenvolvedor Front-End</b>, construindo projetos com Javascript/(React,Next, TypeScript, React Query, Styled Components).
+                                    Venho da área de logística, porém com mais de 2 anos de experiência no desenvolvimento de aplicações web.
+                                    Tenho um olhar aguçado para negócios, novas metodologias.
+                                    Vamos conversar!
+                                </p>
+                                <p>
+                                   Finalizei o primeiro ano do curso da DH em 05/2021 como full stack jr e atualmente faço especialização em Frontend a noite.
+                                </p>
                         </div>
                     </div>           
                 </section>
