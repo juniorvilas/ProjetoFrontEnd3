8d701e5cfb049fab4ca26915322111d4c54dd4c4
Carlindo Silva Queiroz Junior <log_junior@hotmail.com>
2022-09-16 11:21:46 -0300

    Update index.js

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -16,7 +16,7 @@ const Banner = () => {
                         <div>
                             <h2>Sobre mim</h2>
                             <p>
-                                Olá sou Carlindo Junior! <br> Sou <b> Desenvolvedor Front-End </b> , construindo projetos com Javascript/(React,Next, TypeScript, React Query, Styled Components).
+                                Olá sou Carlindo Junior! <br /> Sou <b> Desenvolvedor Front-End </b> , construindo projetos com Javascript/(React,Next, TypeScript, React Query, Styled Components).
                                 Finalizei o primeiro ano do curso da DH em 05/2021 como full stack jr e atualmente faço especialização em Frontend a noite.
                                 Venho da área de logística, porém com mais de 2 anos de experiência no desenvolvimento de aplicações web.
                                 Tenho um olhar aguçado para negócios, novas metodologias.
